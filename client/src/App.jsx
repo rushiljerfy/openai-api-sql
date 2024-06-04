a8e0c25d12a7f6e83330c77301fd1b71097e19fe
Rushil Jerfy <rushiljerfy@rushils-air.lan>
2024-06-03 21:59:11 -0400

    added sql logo, basic input fields without functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,25 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import styles from './index.module.css'
+import sqlLogo from './assets/sql-logo.png'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <h1>hello world
-    </h1>
+    <main className={styles.main}>
+      <img src={sqlLogo} alt = "" className={styles.icon}/>
+      <h3>Generate SQL with AI</h3>
+
+      <form>
+        <input
+        type="text"
+        name="query-description"
+        placeholder="Describe your query"
+        />
+        <input type="submit" value="Generate query"/>
+      </form>
+    </main>
   )
 }
 
