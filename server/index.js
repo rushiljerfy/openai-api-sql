13f60a68ca2d351d877eb883e9541375fc6ca732
Rushil Jerfy <rushiljerfy@Rushils-MacBook-Air.local>
2024-06-06 11:44:47 +0000

    added endpoint + catch error message

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
-import express from "express";
+import express, { query } from "express";
 import cors from "cors";
+import generate from "./generate.js";
 
 const app = express();
+
+app.use(express.json())
 
 // for actual app for production, would have to write whitelist here (ip where you have frontend)
 app.use(cors());
@@ -10,6 +13,17 @@ const port = process.env.PORT || 3005;
 
 app.get("/", (req, res) => {
     res.send("Hello world from our API")
+})
+
+app.post("/generate", async (req, res) => {
+    const queryDescription = req.body.queryDescription
+    try {
+        const sqlQuery = await generate(queryDescription)
+        res.json({ response: sqlQuery })
+    } catch (error) {
+        console.error(error)
+        res.status(500).send("Internal Server Error")
+    }
 })
 
 app.listen(port, () => {
